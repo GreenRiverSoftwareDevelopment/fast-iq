ededcb52ae21f34415e806b233007c005ea2683d
Brian Saylor <bsaylor3@mail.greenriver.edu>
2018-01-17 21:33:25 -0600

    added folders and index require files for individual use

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+    /*
+    Author: Brian, Kevin, Sonie
+    017/16/2018
+    handles routing using fat free framework*/
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+    session_start();
+
+    //require("../config_campsambica.php");
+
+    try{
+        //instantiate a database object
+        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    //Create an instance of the Base class
+    $f3 = Base::instance();
+
+    $f3->set('DEBUG', 3);
+
+    
+    $f3->route('GET /', function($f3) {
+                $view = new View;
+
+                    $usernameCheck = $_SESSION['username'];
+                    $passwordCheck = $_SESSION['password'];
+                    if($usernameCheck == null || $passwordCheck == null){
+                        $f3->reroute('/login');
+                    }
+                echo $view->render('pages/home.html');
+    });
+
+           
+    require("./indexRequire/brian.php");
+    require("./indexRequire/kevin.php");
+    require("./indexRequire/sonie.php");
+
+    //Run fat free
+    $f3->run();
+?>
