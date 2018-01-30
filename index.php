ff4c5f2e21b1d758c64ea1df322e068fa389d9e4
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2018-01-29 22:18:09 -0600

    login

--- index.php
+++ index.php
@@ -9,44 +9,38 @@
     require_once('vendor/autoload.php');
     session_start();
 
-    require("../config_fast-iq.php");
-    
-    try
-    {
-        //instantiate a database object
-        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+    //require("../config_campsambica.php");
+
+    //try{
+    //    //instantiate a database object
+    //    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+    //}
+    //catch(PDOException $e){
+    //    echo $e->getMessage();
+    //}
 
     //Create an instance of the Base class
     $f3 = Base::instance();
 
     $f3->set('DEBUG', 3);
-    $categoryDB = new CategoryDB();
-    $unitDB = new UnitDB();
-    $exerciseDB = new ExerciseDB();
 
     
     $f3->route('GET /', function($f3)
     {
 
-        //$usernameCheck = $_SESSION['username'];
-        //$passwordCheck = $_SESSION['password'];
-        //if($usernameCheck == null || $passwordCheck == null){
-        //    $f3->reroute('/login');
-        //}
-        $view = new View;
+                    //$usernameCheck = $_SESSION['username'];
+                    //$passwordCheck = $_SESSION['password'];
+                    //if($usernameCheck == null || $passwordCheck == null){
+                    //    $f3->reroute('/login');
+                    //}
+                    $view = new View;
                     
-        echo $view->render('pages/login.php');
+                echo $view->render('pages/login.php');
+                //echo Template::instance()->render('pages/login.php');
     });
     
     $f3->route('GET /category', function($f3)
     {
-        //$categories =  $GLOBALS['categoryDB']->allCategories();
-        //$f3->set('categories', $categories);
         echo Template::instance()->render('pages/category_page.html');
     });
     
@@ -62,7 +56,7 @@
     
         $f3->route('GET /login', function($f3)
     {
-        echo Template::instance()->render('pages/login.html');
+        echo Template::instance()->render('pages/login.php');
     });
 
            
--- indexRequire/kevin.php
+++ indexRequire/kevin.php
@@ -2,5 +2,5 @@
 
     $f3->route('GET /login', function($f3)
     {
-        echo Template::instance()->render('pages/login.html');
+        echo Template::instance()->render('pages/login.php');
     });
