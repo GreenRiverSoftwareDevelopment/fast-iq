1b3a09b722e502b21e000f5cacf885b9ff6ace78
Sonie Moon <smoon2@uw.edu>
2018-02-26 12:08:40 -0600

    exercise summary font

--- tmp/3ljqwuhppts00.o6w54iin6alb.php
+++ tmp/3ljqwuhppts00.o6w54iin6alb.php
@@ -6,6 +6,17 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <!-- bootstrap -->
+			<style>
+				p {
+					font-size: medium;  
+			}
+
+				li {
+					font-size: medium;  
+							}
+			</style>
+
+			
                 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
                 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
                     rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
