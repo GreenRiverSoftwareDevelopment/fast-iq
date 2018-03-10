024fa89c0e9bd783aafd56731a8dd0fd12afb82c
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2018-03-10 12:12:38 -0600

    Changed default route to categorySecond fixed password bug. Now function checks if password AND username is in the database before letting them go in the backend.

--- indexRequire/brian.php
+++ indexRequire/brian.php
@@ -99,19 +99,20 @@
         $cost = 10; //Cost of generating has. The higher the value the more secure, but the slower the load of the server.
         $usernameAttempt = $_POST['username'];
         $userPasswordAttempt = $_POST['password'];
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['password'] = $_POST['password'];
+
         
         
         $usergrabbed = $GLOBALS['memberDB']->memberByUsername($usernameAttempt);
         
          $hashPasswordVerify = password_verify($userPasswordAttempt , $usergrabbed['password']);
-        $userExists = $GLOBALS['memberDB']->adminNameExists($usernameAttempt, $hashedPassword);
+        $usernameExists = $GLOBALS['memberDB']->memberByUsername($usernameAttempt);
         
         
        
-        if($hashPasswordVerify)
+        if($hashPasswordVerify && $usernameExists)
         {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['password'] = $_POST['password'];
             $f3->reroute('/categoryBackend');
         }
         else
