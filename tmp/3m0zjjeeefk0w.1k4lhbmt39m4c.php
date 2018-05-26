f1cb52012cec6cfeaa91ee4a962ce102a6119d3e
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2018-05-26 02:28:18 -0500

    added some fixes to the merge conflict, still need to put ajax back in

--- tmp/3m0zjjeeefk0w.1k4lhbmt39m4c.php
+++ tmp/3m0zjjeeefk0w.1k4lhbmt39m4c.php
@@ -98,7 +98,7 @@
 							
 								<div id="searchResults">
 									<?php foreach ((@$exercises?:[]) as $exercise): ?>
-									<a href="#"><div class="panel-body" exerciseNumber="<?= ($exercise['exercise_id']) ?>" id="searchClick" onclick="getSeachClick(this);"><h4> <?= ($exercise['exercise_name']) ?></h4></div></a>
+									<a href="#"><div class="panel-body"><h4> <?= ($exercise['exercise_name']) ?></h4></div></a>
 									<?php endforeach; ?>
 								</div>
 							
@@ -223,25 +223,18 @@
         <br>
 
             <script>
-
 				var summary_view = document.getElementById("summary-view");
 				summary_view.style.display = "none";
-
 				function updateUnits() {
 				  var cat_select = document.getElementById("category_select");
 				  var subcat_select = document.getElementById("unit_select");
 				  var exercise_select = document.getElementById("excercise_select");
-
-
 				  var cat_id = cat_select.options[cat_select.selectedIndex].value;
-
 				  var url = './grabUnits/' + cat_id;
 				  console.log(cat_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					  //cat_select.innerHTML = xhr.responseText;
 					  subcat_select.innerHTML = xhr.responseText;
 					  exercise_select.innerHTML = " ";
@@ -252,28 +245,20 @@
 					  }
 					  $('#collapseOne').collapse('show');
 					  //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				var cat_select = document.getElementById("category_select");
 				cat_select.addEventListener("change", updateUnits);
-
-
 				function updateExcercise() {
 				  var unit_select = document.getElementById("unit_select");
 				  var exercise_select = document.getElementById("excercise_select");
-
 				  var unit_id = unit_select.options[unit_select.selectedIndex].value;
-
 				  var url = './grabExercise/' + unit_id;
 				  console.log(unit_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					  //cat_select.innerHTML = xhr.responseText;
 					  exercise_select.innerHTML = xhr.responseText;
 					  console.log(xhr.responseText);
@@ -281,133 +266,94 @@
 					  {
 						summary_view.style.display = "none";
 					  }
-
-
 					 //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				var unit_select = document.getElementById("unit_select");
 				unit_select.addEventListener("change", updateExcercise);
-
-
 				//Summary
 				function updateSummary() {
 				  var excercise_select = document.getElementById("excercise_select");
 				  var summary_select = document.getElementById("collapseOne");
-
-
 				var summary_view = document.getElementById("summary-view");
-
 				  var summary_id = excercise_select.options[excercise_select.selectedIndex].value;
-
 				  var url = './summaryExercise/' + summary_id;
 				  console.log(summary_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					//cat_select.innerHTML = xhr.responseText;
 					summary_select.innerHTML = xhr.responseText;
 					console.log(xhr.responseText);
 					if(summary_view.style.display === "none")
 					  {
-
 						summary_view.style.display = "block";
 					  }
-
 					//  if(summary_view.style.display === "none")
 					//  {
 					//	summary_view.style.display = "block";
 					//  }
 					 //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				var unit_select = document.getElementById("excercise_select");
 				unit_select.addEventListener("change", updateSummary);
-
-
 				//Video
 				function updateVideo() {
 				  var excercise_select = document.getElementById("excercise_select");
 				  var video_select = document.getElementById("collapseTwo");
-
 				  var video_id = excercise_select.options[excercise_select.selectedIndex].value;
-
 				  var url = './videoExercise/' + video_id;
 				  console.log(video_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					  //cat_select.innerHTML = xhr.responseText;
 					  video_select.innerHTML = xhr.responseText;
 					  console.log(xhr.responseText);
 					 //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				//Questions
 				function updateQuestions() {
 				  var excercise_select = document.getElementById("excercise_select");
 				  var question_select = document.getElementById("collapseThree");
-
 				  var question_id = excercise_select.options[excercise_select.selectedIndex].value;
-
-
-
 				  var url = './questionsExercise/' + question_id;
 				  console.log(question_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					  //cat_select.innerHTML = xhr.responseText;
 					  question_select.innerHTML = xhr.responseText;
 					  console.log(xhr.responseText);
 					 //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				//Picture
 				function updatePicture() {
 				  var excercise_select = document.getElementById("excercise_select");
 				  var picture_select = document.getElementById("collapseFour");
-
 				  var picture_id = excercise_select.options[excercise_select.selectedIndex].value;
-
 				  var url = './pictureExercise/' + picture_id;
 				  console.log(picture_id);
-
 				  var xhr = new XMLHttpRequest();
 				  xhr.open('GET', url, true);
 				  xhr.onreadystatechange = function () {
-
 					  //cat_select.innerHTML = xhr.responseText;
 					  picture_select.innerHTML = xhr.responseText;
 					  //console.log(xhr.responseText);
 					 //subcat_select.style.display = 'inline';
-
 				  };
 				  xhr.send();
 				}
-
 				unit_select.addEventListener("change", updateVideo);
 				unit_select.addEventListener("change", updateQuestions);
 				unit_select.addEventListener("change", updatePicture);
-
 			</script>
 
 	
@@ -429,8 +375,8 @@
 							//console.log(resultFromDatabase);
 							for (searchResult in resultFromDatabase){
 							 var exerciseId = searchResult;
-							 $('#searchResults').append('<div class="panel-body" onclick="getSeachClick(this)" exerciseNumber="'+exerciseId+'" id=searchClick"'+exerciseId+'"><h4>'+resultFromDatabase[exerciseId]['exercise_name']+'</h4></div></a>');
-							 //console.log(searchResult);
+							 $('#searchResults').append('<a href="#"><div class="panel-body"><h4>'+resultFromDatabase[exerciseId]['exercise_name']+'</h4></div></a>');
+							 console.log(searchResult);
 							}
 						}
 						
@@ -439,24 +385,6 @@
 					
 					//console.log(searchInput);
 				}
-				function getSeachClick(searchClickElement)
-				{
-					var exerciseNumber = $(searchClickElement).attr("exerciseNumber");
-					$.ajax({
-						type: "POST",
-						url: "./getSearchClick",
-						dataType: 'html',
-						data: {'exerciseNumber': exerciseNumber},
-						success: function(result)
-						{
-							
-						}
-						
-						
-					});
-					console.log(exerciseNumber);
-				}
-				
 			</script>
             <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
             <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
